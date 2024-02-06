89246b3458aed5efb30da2a2e7f866749b1b228b
Artyom Titov <at@redsteep.com>
2024-02-06 22:55:25 +0300

    Remove generated declarations from Git

--- src/declarations/test_executable/test_executable.did.js
+++
@@ -1,7 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'execute' : IDL.Func([], [], []),
-    'getValue' : IDL.Func([], [IDL.Nat], ['query']),
-  });
-};
-export const init = ({ IDL }) => { return []; };
