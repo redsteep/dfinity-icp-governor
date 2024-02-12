236ace046ada394a15a4cb5065976687366afa26
Artyom Titov <at@redsteep.com>
2024-02-12 22:39:47 +0300

    Fix WASM bundling issue

--- src/hooks/use-candid-parser.ts
+++ src/hooks/use-candid-parser.ts
@@ -1,6 +1,6 @@
 import { Actor, fetchCandid, type HttpAgent } from "@dfinity/agent";
 import { useQuery } from "@tanstack/react-query";
-import init, { parseCandid } from "candid-parser-wasm";
+import { parseCandid } from "candid-parser-wasm";
 import { governor } from "~/declarations/governor";
 import { createDisposableRef } from "~/lib/dispose-on-query-cache-eviction";
 
@@ -9,8 +9,7 @@ export function useCandidParser(canisterId?: string) {
     queryKey: ["candid", canisterId],
     queryFn: async () => {
       const agent = Actor.agentOf(governor) as HttpAgent; // TODO: remove governor reference
-      const source = await fetchCandid(canisterId!, agent);
-      const parser = await init().then(() => parseCandid(source));
+      const parser = parseCandid(await fetchCandid(canisterId!, agent));
       return createDisposableRef(parser, parser.free);
     },
     enabled: Boolean(canisterId),
